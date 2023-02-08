718035b14ed6bc1d033457701baaafe0241f4565
varunsingh1619 <VarunV.Singh@lntinfotech.com>
2023-02-08 12:34:30 +0530

    trial form creation

---
+++ movieTicket/ticketBooking/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Person(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    mobile = models.IntegerField(max_length=255)
+
+    def __str__(self):
+        return self.username
---
+++ movieTicket/ticketBooking/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from ticketBooking.models import Person
+
+# Create your views here.
+
+
+def index(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        number = request.POST['number']
+        print([username,email,password,number])
+        temp = Person(username = username, password = password, email = email, mobile = number)
+        temp.save()
+        print("Data added")
+    return render(request, 'index.html') 
